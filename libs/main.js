eb69ae04913f36a57b1d73a93f89db018568ec0c
Tomoya Koyanagi <tomk79@gmail.com>
2016-07-06 16:00:40 +0900

    考え中。

--- libs/main.js
+++ libs/main.js
@@ -1,5 +1,76 @@
 /**
  * broccoli-processor
  */
-module.exports = function(){
+module.exports = function(options){
+	var _this = this;
+	var fs = require('fs');
+	var Promise = require("es6-promise").Promise;
+	var it79 = require('iterate79');
+	this.options = options;
+
+	/**
+	 * すべてのインスタンスを再帰的に処理する
+	 */
+	this.each = function(each, callback){
+		callback = callback || function(){};
+		// console.log(this.options);
+		var json = fs.readFileSync(this.options.realpathDataDir+'/data.json').toString();
+		json = JSON.parse(json);
+		console.log(json);
+
+		function instanceProcess( row, idx, callback ){
+			console.log(row);
+			console.log(idx);
+
+			it79.ary(
+				row.fields,
+				function( it1, childFields, childsIdx ){
+					it79.ary(
+						childFields,
+						function( it2, childField, childIdx ){
+							instanceProcess(
+								childField, childIdx,
+								function(){
+									it2.next();
+								}
+							);
+						},
+						function(){
+							callback();
+						}
+					);
+				},
+				function(){
+					callback();
+				}
+			);
+
+		}
+
+		it79.ary(
+			json,
+			function( it1, row, idx ){
+				it79.ary(
+					row,
+					function( it2, row2, idx2 ){
+						instanceProcess(
+							row2, idx2,
+							function(){
+								it2.next();
+							}
+						);
+					},
+					function(){
+						callback();
+					}
+				);
+			},
+			function(){
+				callback();
+			}
+		);
+
+		return;
+	}
+
 }
--- tests/mainTest.js
+++ tests/mainTest.js
@@ -3,11 +3,31 @@ var path = require('path');
 var fs = require('fs');
 var Promise = require("es6-promise").Promise;
 
-describe('test', function() {
+var BroccoliProcessor = require("../libs/main.js");
 
-	it("test", function(done) {
+describe('再帰検索のテスト', function() {
+
+	it("再帰的に検索する", function(done) {
 		this.timeout(60*1000);
-		assert.equal(1, 1);
-        done();
+
+		var broccoliProcessor = new BroccoliProcessor({
+			'paths_module_template': {},
+			'documentRoot': path.resolve(__dirname, 'testdata/page1/')+'/',
+			'pathHtml': '/page1.html',
+			'pathResourceDir': '/page1_files/resources/',
+			'realpathDataDir': path.resolve(__dirname, 'testdata/page1/page1_files/guieditor.ignore/')+'/'
+		});
+		broccoliProcessor.each(
+			function( data, next ){
+				console.log(data);
+				next();
+			},
+			function(){
+				console.log('finished.');
+				assert.equal(1, 1);
+			}
+		);
+
+		done();
 	});
 });
