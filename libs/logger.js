2733d671a5d12f9ed5432a5991f98351db21b48d
Tomoya Koyanagi <tomk79@gmail.com>
2018-08-02 23:37:07 +0900

    logger が値を toString() しないようになった

--- libs/logger.js
+++ libs/logger.js
@@ -4,30 +4,12 @@
 module.exports = function(){
 	var logs = {};
 
-	function toString(val, depth){
-		depth = depth || 1;
-		switch( typeof(val) ){
-			case typeof(''):
-				return val;break;
-			case typeof(true):
-			case typeof(0):
-				return ''+val;break;
-			case typeof(function(){}):
-				return '[Function]';break;
-			case typeof({}):
-				return JSON.stringify(val);break;
-			default:
-				return ''+val;break;
-		}
-		return val;
-	}
-
 	/** メッセージを記録する */
 	this.log = function( instancePath, message ){
 		if( !logs[instancePath] ){
 			logs[instancePath] = [];
 		}
-		logs[instancePath].push(toString(message));
+		logs[instancePath].push(message);
 		return true;
 	}
 
